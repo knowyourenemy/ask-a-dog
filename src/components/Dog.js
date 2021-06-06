84dfcd8f5b7818699926915b267724bcc0ba4b1a
Aaditya Patwari <knowyourenemy@users.noreply.github.com>
2021-06-06 21:26:02 +0800

    add screen capture feature

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -4,6 +4,13 @@ import './Dog.css'
 import Button from './Button'
 import { quotes } from './Quotes.js'
 import dog_bone_icon from '../assets/images/dog-bone-6453.svg';
+import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg} from 'html-to-image';
+import download from 'downloadjs'
+import ball_icon from '../assets/images/Picture 1.png';
+import domtoimage from 'dom-to-image';
+import Base64Downloader, { triggerBase64Download } from 'react-base64-downloader';
+import toImg from 'react-svg-to-image';
 
 function Dog(props) {
     const [dog, setDog] = useState("")
@@ -12,6 +19,29 @@ function Dog(props) {
     const [isDogImgLoading, setIsDogImgLoading] = useState(false)
     const [quote, setQuote] = useState({})
     const [isLoadingError, setIsLoadingError] = useState(false)
+    const node = document.getElementById('my-node');
+    const [isSharing, setIsSharing] = useState(false)
+
+
+    const onShareClick = () => {
+        setIsSharing(true)
+        document.getElementById('form').style.display = "none"
+        htmlToImage.toPng(document.getElementById('home'))
+        .then(function (dataUrl) {
+            download(dataUrl, 'dogwisdom');
+
+            document.getElementById('form').style.display = "flex"
+            setIsSharing(false)
+            //var img = new Image();
+            //img.src = dataUrl;
+            //document.body.appendChild(img);
+            // toImg('svg', 'name').then(fileData => {
+            //     //do something with the data
+            //     console.log("success!!!!")
+            //   });
+              
+        });
+    }
 
 
 
@@ -47,14 +77,14 @@ function Dog(props) {
     }
 
     return (
-        <div className='dog-wrapper'>
+        <div className='dog-wrapper' id='my-node'>
             <div className='dog-question'>
                 {props.question}
             </div>
             {isDogUrlLoading ? (
                 <div className='dog-loading-1' style={{ display: isLoadingError ? "none" : "flex" }}>Consulting Dogfather...</div>
             ) : (
-                <div className='dog-loaded'>
+                <div className='dog-loaded' style={{ display: isLoadingError ? "none" : "flex" }}>
                     <div className='dog-content' style={{ display: isDogImgLoading ? "none" : "flex" }}>
                         <img src={dog} className='dog-img' onLoad={() => setIsDogImgLoading(false)}>
 
@@ -68,9 +98,9 @@ function Dog(props) {
                             </div>
 
                         </div>
-
-                        <div className='dog-button-container' style={{ display: isDogImgLoading ? "none" : "flex" }}>
-                            <Button onClick={onRefreshClick} text='fetch another!' icon={dog_bone_icon}/>
+                        <div className='dog-button-container' style={{ display: isDogImgLoading || isLoadingError ? "none" : "flex" , visibility: isSharing ? "hidden" : "visible" }}>
+                            <Button onClick={onRefreshClick} text='fetch another!' icon={dog_bone_icon} />
+                            <Button onClick={onShareClick} text='save wisdom!' icon={ball_icon}/>
                         </div>
                     </div>
                     <div className='dog-loading-2' style={{ display: isDogImgLoading && !isLoadingError ? "flex" : "none" }}>Sacrificing Treat...</div>
